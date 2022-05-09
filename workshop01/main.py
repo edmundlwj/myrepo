5f312b5b8cc143de352c83d5b52f5f42f1a9908c
edmundlwj <edmundlwj@gmail.com>
2022-05-09 09:28:31 -0700

    all 3 tasks completed and added .gitignore file

--- workshop01/main.py
+++ workshop01/main.py
@@ -1,35 +1,19 @@
-from flask import Flask
-from flask import request
+from flask import Flask, render_template
+import os, random
 
+IMAGE_FOLDER = os.path.join('static')
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
 
-
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    try:
-        fahrenheit = float(celsius) * 9 / 5 + 32
-        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-        return str(fahrenheit)
-    except ValueError:
-        return "invalid input"
-
+def random_sentence():
+    sentences = ["Logic will get you from A to B. Imagination will take you everywhere.", "There are 10 kinds of people. Those who know binary and those who dont.", "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.", "It\'s not that I'm so smart, it\'s just that I stay with problems longer.", "It is pitch dark. You are likely to be eaten by a grue."]
+    return str(random.choice(sentences))
 
 @app.route("/")
 def index():
-    celsius = request.args.get("celsius", "")
-    if celsius:
-        fahrenheit = fahrenheit_from(celsius)
-    else:
-        fahrenheit = ""
-    return (
-        """<form action="" method="get">
-                Celsius temperature: <input type="text" name="celsius">
-                <input type="submit" value="Convert to Fahrenheit">
-            </form>"""
-        + "Fahrenheit: "
-        + fahrenheit
-    )
-
+        full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'kisspng-fortune-cookie-biscuits-chocolate-chip-cookie-chin.jpg')
+        r_sentence = random_sentence()
+        return render_template('index.html', user_image=full_filename, sentence=r_sentence)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
